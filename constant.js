4c49e9b48a9940ed319f2a980d7575d9f4b72f36
Mayankpateldev <47097268+Mayankpateldev@users.noreply.github.com>
2019-01-28 15:33:34 +0530

    Update constant.js

--- constant.js
+++ constant.js
@@ -2,5 +2,6 @@ module.exports = {
     COUCHDB_HOST: '127.0.0.1',
     COUCHDB_PORT: '5984',
     COUCHDB_PROTOCOL: 'http',
-    DATABASE_NAME: "vaultdragon"
+    DATABASE_NAME: "vaultdragon",
+    SERVER_URL:'http://127.0.0.1:3001'
 };
