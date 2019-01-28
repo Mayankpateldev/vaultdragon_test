3dcdd4c21b8bb744a8ce2aa14ddd78568a111339
Mayankpateldev <47097268+Mayankpateldev@users.noreply.github.com>
2019-01-28 15:57:09 +0530

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -2,10 +2,12 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
 var server = require("../bin/www");
+console.log(server);
 chai.use(chaiHttp);
-const serverURL = "http://localhost:3001";
+const serverURL = require("../constant").SERVER_URL;
 
 describe('vaultDragon', function () {
+
     it("should add a SINGLE value on /api/create POST", function (done) {
         chai.request(serverURL)
             .post('/api/create')
@@ -47,27 +49,55 @@ describe('vaultDragon', function () {
             });
     });
 
-    it('should list latest records on /api/find GET', function(done) {
+    it('should list latest records on /api/find GET', function (done) {
         chai.request(serverURL)
-          .get('/api/find/123')
-          .end(function(err, res){
-            res.should.have.status(200);
-            done();
-          });
+            .get('/api/find/123')
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
     });
 
-    it('cannot GET /api/find with invalid key', function(done) {
+    it('cannot GET /api/find with invalid key', function (done) {
         chai.request(serverURL)
-          .get('/api/find')
-          .end(function(err, res){
-            res.should.have.status(500);
-            done();
-          });
+            .get('/api/find')
+            .end(function (err, res) {
+                res.should.have.status(422);
+                done();
+            });
     });
 
-});
+    it('cannot GET /api/find with an invalid timestamp', function (done) {
+        chai.request(serverURL)
+            .get('/api/find/abc?timestamp=adadad')
+            .end(function (err, res) {
+                res.should.have.status(422);
+                done();
+            });
+    })
 
-describe('404 Test Case', function(){
+    it('can GET /api/find with valid timestamp', function (done) {
+        const timestamp = '1548670784';
+        chai.request(serverURL)
+            .get(`/api/find/123?timestamp=${timestamp}`)
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    })
+
+    it('cannot GET /api/find with valid but older timestamp', function (done) {
+        const timestamp = '1517134781';
+        chai.request(serverURL)
+            .get(`/api/find/123?timestamp=${timestamp}`)
+            .end(function (err, res) {
+                res.should.have.status(204);
+                done();
+            })
+
+    });
+})
+describe('404 Test Case', function () {
     it("should return 404 not found", function (done) {
         chai.request(serverURL)
             .get('/test')
@@ -76,7 +106,4 @@ describe('404 Test Case', function(){
                 done();
             });
     })
-    it('should return 404', async ()=>{
-      await request(serverURL).get('/test').expect(404);
-    })
-  })
+})
